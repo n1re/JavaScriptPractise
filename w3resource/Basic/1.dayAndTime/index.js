c1c2dfca9caa925b0339018378f5eef93ee2e551
Nikita <mgfile08@gmail.com>
2019-06-04 16:56:14 +0500

    made 1 task

--- w3resource/Basic/1.dayAndTime/index.js
+++ w3resource/Basic/1.dayAndTime/index.js
@@ -19,7 +19,21 @@ function log(time) {
   const date = new Date(time || Date.now());
   const dayIndex = date.getDay();
   const dayString = DAYS[dayIndex];
-  console.log(dayString);
+
+  console.log(`Today is : ${dayString}.`);
+
+  let hours = date.getHours();
+  let half = 'AM'
+  
+  if (hours > 12) {
+    hours -= 12;
+    half = 'PM';
+  }
+
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  console.log(`Current time is : ${hours} ${half} : ${minutes} : ${seconds}`);
 }
 
 log();

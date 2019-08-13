caa16c095915b4c61ee2d47edf4c3a197ccd7d15
Nikita <mgfile08@gmail.com>
2019-08-13 17:12:56 +0500

    partialApplication

---
+++ YDKJS/this/binding/partialApplication.js
@@ -0,0 +1,9 @@
+const assert = require('assert').strict
+
+function summTwoValues(a, b) {
+    this.val = a + b
+}
+
+const summTwoValuesWithPartialApplication = summTwoValues.bind(null, 1, 2)
+
+assert((new summTwoValuesWithPartialApplication()).val === 3)

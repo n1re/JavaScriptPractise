332e159f3df082c800a3cb08f1cb1ae725e2ef8a
Nikita <mgfile08@gmail.com>
2019-08-14 15:43:18 +0500

    repeated arrow functions

---
+++ YDKJS/this/binding/arrowFunctions.js
@@ -0,0 +1,39 @@
+const assert = require('assert').strict;
+
+function foo() {
+	// возвращаем стрелочную функцию
+	return () => {
+		// Здесь `this` лексически заимствован из `foo()`
+		return this.a
+	}
+}
+
+const obj = {
+	a: 2
+};
+
+global.a = 3
+
+const bar = foo.call( obj )
+
+assert(bar() === 2)
+
+function someTimeOutedFunc() {
+    setTimeout(() => {
+        assert(this.a === 2)
+    }, 100)
+}
+
+someTimeOutedFunc.call(obj)
+
+// under ES6 version of upper pattern
+
+function anotherTimeOutedFunc() {
+    var self = this;
+
+    setTimeout(() => {
+        assert(self.a === 2);
+    });
+}
+
+anotherTimeOutedFunc.call(obj)

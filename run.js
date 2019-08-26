34f15da4bbd8ae37b969f399cf84294db40974f1
Nikita <mgfile08@gmail.com>
2019-08-26 19:50:05 +0500

    lint fixes

--- run.js
+++ run.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs')
 
-const path = process.argv[2];
+const path = process.argv[2]
 
 
 /**
@@ -13,7 +13,7 @@ const assert = require('assert').strict;
 if (path) {
     const code = fs.readFileSync(path).toString('utf-8')
 
-    eval(code);
+    eval(code)
 } else {
     throw new Error('Need a path for a file to execute passed as 3rd command line argument')
 }

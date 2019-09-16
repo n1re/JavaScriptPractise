951d8e08b9576ba956659df024bcfb25b9e5f9d3
nireone <mgfile08@gmail.com>
2019-09-16 12:58:30 +0500

    rename

--- utils/array-to-kebab.js
+++ utils/array-to-kebab.js
@@ -1,12 +1,12 @@
 function arrayToCamel(array) {
-    const toSetFirst = array.map(word => {
+    const mapped = array.map(word => {
         const uppered = word[0].toUpperCase();
         const replaced = word.replace(word[0], uppered);
         
         return replaced;
     });
 
-    return toSetFirst.join('');
+    return mapped.join('');
 }
 
 assert(arrayToCamel(['aass', 'ssaa']) === 'AassSsaa');

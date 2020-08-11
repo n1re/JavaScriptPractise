1a95dc8d0c31ed28d212bbf3316cbd4a6a7973b9
Nikita Ananyev <mgfile08@gmail.com>
2020-08-11 05:20:16 +0500

    made  flattern algorythm

---
+++ AlgorithmsAndDataStructures/flattern.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const assert = require('assert');
+
+function flatten(list) {
+  const output = [];
+
+  function _insertToOutput(arr) {
+    for (const el of arr) {
+      if (Array.isArray(el)) {
+        _insertToOutput(el);
+      } else {
+        output.push(el);
+      }
+    }
+  }
+
+  _insertToOutput(list);
+  
+  return output;
+}
+
+const func = function() {};
+const obj = { a: 1 };
+
+assert.deepEqual(
+    flatten([1, 'any [complex] string', null, func, [1, 2, [3, '4'], 0], [], obj]),
+    [1, 'any [complex] string', null, func, 1, 2, 3, '4', 0, obj]
+);
